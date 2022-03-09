2529ae4935e4286d79ae86d97fed77437d406198
Estevan Gonzalez <estevangonzalez80@gmail.com>
2022-03-08 16:25:38 -0800

    Updated average of data values

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,6 +7,7 @@ const controller = {
       var page = req.query.page || 1;
       var count = req.query.count || 5;
       var sort = req.query.sort || 'newest';
+      const start = Date.now()
 
       if (sort === 'newest') {
         sort = `ORDER BY review_date ASC`
@@ -38,7 +39,7 @@ const controller = {
           reviews.review_date, reviews.reviewer_name, reviews.helpfulness
           ${sort} LIMIT $2 OFFSET ${count * page - count};
           `, [product_id, count])
-
+          const duration = Date.now() - start;
           var response = {
             product: product_id,
             page: page,
@@ -47,7 +48,7 @@ const controller = {
               return row.json_build_object
             })
           }
-          console.log(reviews)
+            console.log("Executed Querie", {duration, rowCount: reviews.rowCount})
         res.status(200).send(response)
       }
       catch (err) {
@@ -58,15 +59,17 @@ const controller = {
     addReview: async (req,res) => {
       var currentDate = Math.round((new Date()).getTime() / 1000)
       var {product_id, rating, summary, body, recommend, reviewer_name, reviewer_email, photos, characteristics} = req.body
+      const start = Date.now()
       try {
         var add = await pool.query(
           `INSERT INTO reviews
           (product_id, rating, review_date, summary, body, recommend, reviewer_name, reviewer_email)
           VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING review_id, rating`
         ,[product_id, rating, currentDate, summary, body, recommend, reviewer_name, reviewer_email])
-          .then(async ({rows}) => {
-            var rating = rows[0].rating
-            var review_id = rows[0].review_id;
+          .then(async (rows) => {
+
+            var rating = rows.rows[0].rating
+            var review_id = rows.rows[0].review_id;
             if (photos.length > 0 ) {
               await photos.forEach(url_tag => {
                 pool.query(
@@ -81,6 +84,8 @@ const controller = {
                 (char_id, review_id, data_value)
                 VALUES ($1, $2, $3)`, [id, review_id, rating])
             }
+            const duration = Date.now() - start;
+            console.log("Executed Querie", {duration, rowCount: rows.rowCount})
             res.status(200).send('SUCCESS POSTING')
           })
       }
@@ -91,9 +96,10 @@ const controller = {
 
     getMetaData: async (req, res) => {
       var { product_id } = req.query
+      const start = Date.now()
       try {
         const meta = await pool.query(
-         `SELECT reviews.product_id,
+         ` SELECT reviews.product_id,
                    (SELECT json_build_object(
                      1, (SELECT count(*) filter (where rating = 1)),
                      2, (SELECT count(*) filter (where rating = 2)),
@@ -101,17 +107,17 @@ const controller = {
                      4, (SELECT count(*) filter (where rating = 4)),
                      5, (SELECT count(*) filter (where rating = 5))
                     )) AS ratings,
-                   (SELECT json_build_object(
-                     true, (SELECT count(reviews.recommend) filter (where recommend = true)),
-                     false, (SELECT count(reviews.recommend) filter (where recommend = false))
-                   )) AS recommended,
-                    (SELECT json_object_agg(prod_name, json_build_object('id', characteristics.char_id, 'value', meta_data.data_value)) AS characteristics
-                    FROM characteristics JOIN meta_data ON characteristics.char_id = meta_data.data_id AND reviews.product_id = characteristics.product_id)
+                    (SELECT json_object_agg(prod_name,
+                      json_build_object('id', characteristics.char_id,
+                      'value', (SELECT CAST(avg(data_value) AS TEXT) FROM meta_data WHERE char_id = characteristics.char_id)))
+                      AS characteristics FROM characteristics
+                      JOIN meta_data ON characteristics.char_id = meta_data.data_id AND reviews.product_id = characteristics.product_id)
                    FROM reviews
                    WHERE reviews.product_id = $1
             GROUP BY reviews.product_id;`
         ,[product_id])
-
+        const duration = Date.now() - start;
+        console.log("Executed Querie", {duration, rowCount: meta.rowCount})
         res.status(200).send(meta.rows)
       }
       catch (err) {
@@ -122,12 +128,15 @@ const controller = {
 
     helpfulReview: async (req, res) => {
       const { review_id } = req.params;
+      const start = Date.now()
       try {
         const helpful = await pool.query
         (`
           UPDATE reviews SET helpfulness = CAST(helpfulness AS integer) + 1 WHERE review_id = $1
         `,[review_id]
         )
+        const duration = Date.now() - start;
+        console.log("Executed Querie", {duration, rowCount: helpful.rowCount})
         res.status(204).send(helpful.rows[0])
       }
       catch (err) {
@@ -138,11 +147,14 @@ const controller = {
 
     reportReview: async (req, res) => {
       const { review_id } = req.params;
+      const start = Date.now()
       try {
         const reporter = await pool.query(
         `
           UPDATE reviews SET reported = true WHERE review_id = $1
         `,[review_id])
+        const duration = Date.now() - start;
+        console.log("Executed Querie", {duration, rowCount: reporter.rowCount})
         res.status(204).send(reporter.rows[0])
       }
       catch (err) {
