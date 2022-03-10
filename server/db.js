13d6767b9a4e9593ef2ebdb263d4f31871c237f3
Estevan Gonzalez <estevangonzalez80@gmail.com>
2022-03-09 18:33:26 -0800

    Fixing bugs

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const pool = new Pool({
   host: process.env.HOST || "54.241.172.189",
   user: process.env.USER || "postgres",
-  password: process.env.PASSWORD || "notmyname"
+  password: process.env.PASSWORD || "notmyname",
   database: process.env.DATABASE || "reviews",
   port: process.env.PORT || 5432,
 })
