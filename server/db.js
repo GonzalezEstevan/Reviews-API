e8d1de5918c7b6c4370479623a43e9fb058eae12
Estevan Gonzalez <estevangonzalez80@gmail.com>
2022-03-10 11:48:44 -0800

    Updated

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 require('dotenv').config()
 
 const pool = new Pool({
-  host: process.env.HOST || "54.241.172.189",
+  host: process.env.HOST || "172.31.5.129",
   user: process.env.USER || "postgres",
   password: process.env.PASSWORD || "notmyname",
   database: process.env.DATABASE || "reviews",
